266a5fb372b0fdfbcdfd463b58432d467eebda9f
jahwankoo <jahwankoo@gmail.com>
2023-10-02 04:37:40 +0900

    Update mnist-test.py

--- mnist-test.py
+++ mnist-test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import tensorflow as tf  # 다른 ML 라이브러리를 사용한다면 변경
 import numpy as np
-from PIL import Image
+from PIL import ImageOps
 
 # 미리 훈련된 모델 불러오기
 model = tf.keras.models.load_model('my_model.h5')
@@ -13,14 +13,14 @@ uploaded_image = st.file_uploader("손글씨 문자를 업로드하세요", type
 
 if uploaded_image is not None:
     image = Image.open(uploaded_image)
-    st.image(image, caption="업로드한 문자", use_column_width=True)
+    image = ImageOps.grayscale(image)  # 채널을 1개로 변경
+    st.image(image, caption="Uploaded Character", use_column_width=True)
 
     # 이미지 전처리 및 예측
-    image = np.array(image.resize((28, 28)))  # 사이즈와 전처리를 모델에 맞게 조정
+    image = np.array(image.resize((28, 28)))  # 사이즈 조절
     image = image / 255.0  # 정규화
-    image = np.expand_dims(image, axis=0)  # 배치 차원 추가
+    image = image.reshape(1, 784)  # 차원을 (None, 784)로 변경
 
     predictions = model.predict(image)
     class_idx = np.argmax(predictions[0])
-
-    st.write(f"예측: 클래스 {class_idx}")
+    st.write(f"Prediction: Class {class_idx}")
